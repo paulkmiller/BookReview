f65fa26a933d1e37a0f8939293b61ac66e481365
Paul Miller <pmillerk88@gmail.com>
2017-04-25 16:47:06 -0400

    Show page now uses book id to show the book title, description, and author using /book/:id as path

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < ApplicationController
+  # before_action specifies something we want to do before any of the following actions/mothods are used / initiated
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all.order("created_at DESC")
   end
 
   def new
     @book = Book.new
+  end
+
+  def show
   end
 
   def create
@@ -21,6 +27,10 @@ class BooksController < ApplicationController
 
     def book_params
       params.require(:book).permit(:title, :description, :author)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
     end
 
 end
